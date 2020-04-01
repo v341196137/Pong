ce5c189ab44725dd6ff34b7cb2f2b7ee1650a6ca
v341196137 <38384400+v341196137@users.noreply.github.com>
2020-04-01 13:27:36 -0400

    Update dnfldkfsf.py

--- dnfldkfsf.py
+++ dnfldkfsf.py
@@ -24,8 +24,8 @@ ballX = 400 - (ballSize/2)
 ballY = 300 - (ballSize/2)
 ballSpeed = 1
 angle = random.random()*3
-ballDirY = randrange(-1, 1, 2)
-ballDirX = randrange(-1, 1, 2)
+ballDirY = randrange(-1, 2, 2)
+ballDirX = randrange(-1, 2, 2)
 r = 255
 g = 0
 b = 0
@@ -111,6 +111,8 @@ while 1:
                     player1Direction = 0
                 elif (event.key == pygame.K_i) or (event.key == pygame.K_k):
                     player2Direction = 0
+        elif gameMode == "winScreen":
+            gameMode = "menu"
 
     if gameMode == "menu":
         #text
@@ -119,7 +121,7 @@ while 1:
         play = buttonComicSans.render("Play!", 0, (r, g, b))
         screen.blit(play, (360, 195))
         #button outlines
-        pygame.draw.rect(screen, (r, g, b), (350, 200, 100, 50), 1)        
+        pygame.draw.rect(screen, (r, g, b), (350, 200, 100, 50), 1)
     elif gameMode == "game":
         #ball movement
         ballX += ballSpeed*ballDirX
@@ -152,15 +154,8 @@ while 1:
         #score check
         score = comicSans.render("Score\n" + str(player1Score) + ":" + str(player2Score), 0, (r, g, b))
         screen. blit(score, (350, 0))
-        if player1Score == 7:
-            ballSpeed = 0
-            winMessage = comicSans.render("Player 1 wins!", 0, (r, g, b))
-            screen.blit(winMessage, (350, 270))
-        elif player2Score == 7:
-            ballSpeed = 0
-            winMessage = comicSans.render("Player 2 wins!", 0, (r, g, b))
-            screen.blit(winMessage, (350, 270))
-            
+        if (player1Score == 7) or (player2Score == 7):
+            gameMode = "winScreen"
         #draw stuff based on theme assigned by date
         if (theme == "default") or (theme == "aprfools"):
             pygame.draw.ellipse(screen, (r, g, b), (ballX, ballY, ballSize, ballSize), 1)
@@ -170,6 +165,15 @@ while 1:
             screen.blit(ballImage, (ballX, ballY))
             screen.blit(paddleImage, (40, player1Pos))
             screen.blit(paddleImage, (720, player2Pos))
+    elif gameMode == "winScreen":
+        if player1Score == 7:
+            winMessage = comicSans.render("Player 1 wins!", 0, (r, g, b))
+            screen.blit(winMessage, (325, 270))
+        else:
+            winMessage = comicSans.render("Player 2 wins!", 0, (r, g, b))
+            screen.blit(winMessage, (325, 270))
+        playAgainMessage = comicSans.render("Click anywhere or any  key to return to the main menu", 0, (r, g, b))
+        screen.blit(playAgainMessage, (150, 400))
     #change colours
     r+=dr
     g+=dg
