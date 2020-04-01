fd24098a300bb4e1ef73bd32a2baf33b354d767b
v341196137 <38384400+v341196137@users.noreply.github.com>
2020-04-01 13:08:30 -0400

    Update dnfldkfsf.py

--- dnfldkfsf.py
+++ dnfldkfsf.py
@@ -34,16 +34,20 @@ dg = 1
 db = 0
 month, day = datetime.datetime.now().month, datetime.datetime.now().day
 comicSans = pygame.font.SysFont("Comic Sans MS", 20)
-screen = pygame.display.set_mode((800, 600))
+buttonComicSans = pygame.font.SysFont("Comic Sans MS", 40)
+titleComicSans = pygame.font.SysFont("Comic Sans MS", 60)
+gameMode = "menu"
 theme = ""
 ballImage = 0
 paddleImage = 0
+screen = pygame.display.set_mode((800, 600))
 
 # Functions
 # startGame
 # @param ballX, reset x position of the ball back to the middle
 # @param ballY, resets y position of the ball back to the middle
 # @param ballSpeed, puts the ball's speed back to 1
+# @param angle, generates a random angle for the ball
 # @return returns all the values back to be reassigned because global variables are bad
 def startGame(ballX, ballY, ballSpeed, angle):
     ballX = 400 - (ballSize/2)
@@ -81,76 +85,91 @@ while 1:
     elif (theme == "christmas"):
         screen.fill((200, 230, 255))
 
-    #handle i/o
+    #handle i/o, different i/o based on mode
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_w:
-                player1Direction = -1
-            elif event.key == pygame.K_s:
-                player1Direction = 1
-            elif event.key == pygame.K_i:
-                player2Direction = -1
-            elif event.key == pygame.K_k:
-                player2Direction = 1
-        elif event.type == pygame.KEYUP:
-            if (event.key == pygame.K_w) or (event.key == pygame.K_s):
-                player1Direction = 0
-            elif (event.key == pygame.K_i) or (event.key == pygame.K_k):
-                player2Direction = 0
+        if gameMode == "menu":
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mouseX, mouseY = pygame.mouse.get_pos()
+                if (mouseX >= 350) and (mouseX <= 450):
+                    if(mouseY >= 200) and (mouseY <= 250):
+                        gameMode = "game"
+        elif gameMode == "game":
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_w:
+                    player1Direction = -1
+                elif event.key == pygame.K_s:
+                    player1Direction = 1
+                elif event.key == pygame.K_i:
+                    player2Direction = -1
+                elif event.key == pygame.K_k:
+                    player2Direction = 1
+            elif event.type == pygame.KEYUP:
+                if (event.key == pygame.K_w) or (event.key == pygame.K_s):
+                    player1Direction = 0
+                elif (event.key == pygame.K_i) or (event.key == pygame.K_k):
+                    player2Direction = 0
 
-    
-    #ball movement
-    ballX += ballSpeed*ballDirX
-    ballY += angle*ballDirY
-    #ball bounce
-    if ballY <= 0 or ballY >= 600-ballSize:
-        ballDirY *= -1
-    #score points
-    if ballX <= 0:
-        player2Score += 1
-        ballX, ballY, ballSpeed, angle = startGame(ballX, ballY, ballSpeed, angle)
-    elif ballX >= 800-ballSize:
-        player1Score += 1
-        ballX, ballY, ballSpeed, angle = startGame(ballX, ballY, ballSpeed, angle)
+    if gameMode == "menu":
+        #text
+        title = titleComicSans.render("Pong!", 0, (r, g, b))
+        screen.blit(title, (325, 20))
+        play = buttonComicSans.render("Play!", 0, (r, g, b))
+        screen.blit(play, (360, 195))
+        #button outlines
+        pygame.draw.rect(screen, (r, g, b), (350, 200, 100, 50), 1)        
+    elif gameMode == "game":
+        #ball movement
+        ballX += ballSpeed*ballDirX
+        ballY += angle*ballDirY
+        #ball bounce
+        if ballY <= 0 or ballY >= 600-ballSize:
+            ballDirY *= -1
+        #score points
+        if ballX <= 0:
+            player2Score += 1
+            ballX, ballY, ballSpeed, angle = startGame(ballX, ballY, ballSpeed, angle)
+        elif ballX >= 800-ballSize:
+            player1Score += 1
+            ballX, ballY, ballSpeed, angle = startGame(ballX, ballY, ballSpeed, angle)
 
-    #recieve ball
-    if (abs(ballX - 60 <= ballSpeed)) and (ballY + ballSize >= player1Pos) and (ballY <= player1Pos + paddleSize):
-        ballDirX = 1
-        angle = ((ballY - (player1Pos + (paddleSize/2)))/paddleSize)*3
-    elif (abs(720 - ballSize - ballX <= ballSpeed)) and (ballY + ballSize >= player2Pos) and (ballY <= player2Pos + paddleSize):
-        ballDirX = -1
-        angle = ((ballY - (player2Pos + (paddleSize/2)))/paddleSize)*3
+        #recieve ball
+        if (abs(ballX - 60 <= ballSpeed)) and (ballY + ballSize >= player1Pos) and (ballY <= player1Pos + paddleSize):
+            ballDirX = 1
+            angle = ((ballY - (player1Pos + (paddleSize/2)))/paddleSize)*3
+        elif (abs(720 - ballSize - ballX <= ballSpeed)) and (ballY + ballSize >= player2Pos) and (ballY <= player2Pos + paddleSize):
+            ballDirX = -1
+            angle = ((ballY - (player2Pos + (paddleSize/2)))/paddleSize)*3
 
-    #update player positions
-    if (player1Pos > 0 and player1Pos < 600 - paddleSize) or (player1Pos <= 0 and player1Direction == 1) or (player1Pos >= 600 - paddleSize and player1Direction == -1):
-        player1Pos += player1Direction
-    if (player2Pos > 0 and player2Pos < 600 - paddleSize) or (player2Pos <= 0 and player2Direction == 1) or (player2Pos >= 600 - paddleSize and player2Direction == -1):
-        player2Pos += player2Direction
+        #update player positions
+        if (player1Pos > 0 and player1Pos < 600 - paddleSize) or (player1Pos <= 0 and player1Direction == 1) or (player1Pos >= 600 - paddleSize and player1Direction == -1):
+            player1Pos += player1Direction
+        if (player2Pos > 0 and player2Pos < 600 - paddleSize) or (player2Pos <= 0 and player2Direction == 1) or (player2Pos >= 600 - paddleSize and player2Direction == -1):
+            player2Pos += player2Direction
 
-    #score check
-    score = comicSans.render("Score\n" + str(player1Score) + ":" + str(player2Score), 0, (r, g, b))
-    screen. blit(score, (350, 0))
-    if player1Score == 7:
-        ballSpeed = 0
-        winMessage = comicSans.render("Player 1 wins!", 0, (r, g, b))
-        screen.blit(winMessage, (350, 270))
-    elif player2Score == 7:
-        ballSpeed = 0
-        winMessage = comicSans.render("Player 2 wins!", 0, (r, g, b))
-        screen.blit(winMessage, (350, 270))
-        
-    #draw stuff based on theme assigned by date
-    if (theme == "default") or (theme == "aprfools"):
-        pygame.draw.ellipse(screen, (r, g, b), (ballX, ballY, ballSize, ballSize), 1)
-        pygame.draw.rect(screen, (r, g, b), (40, player1Pos, 20, paddleSize), 1)
-        pygame.draw.rect(screen, (r, g, b), (720, player2Pos, 20, paddleSize), 1)
-    else:
-        screen.blit(ballImage, (ballX, ballY))
-        screen.blit(paddleImage, (40, player1Pos))
-        screen.blit(paddleImage, (720, player2Pos))
+        #score check
+        score = comicSans.render("Score\n" + str(player1Score) + ":" + str(player2Score), 0, (r, g, b))
+        screen. blit(score, (350, 0))
+        if player1Score == 7:
+            ballSpeed = 0
+            winMessage = comicSans.render("Player 1 wins!", 0, (r, g, b))
+            screen.blit(winMessage, (350, 270))
+        elif player2Score == 7:
+            ballSpeed = 0
+            winMessage = comicSans.render("Player 2 wins!", 0, (r, g, b))
+            screen.blit(winMessage, (350, 270))
+            
+        #draw stuff based on theme assigned by date
+        if (theme == "default") or (theme == "aprfools"):
+            pygame.draw.ellipse(screen, (r, g, b), (ballX, ballY, ballSize, ballSize), 1)
+            pygame.draw.rect(screen, (r, g, b), (40, player1Pos, 20, paddleSize), 1)
+            pygame.draw.rect(screen, (r, g, b), (720, player2Pos, 20, paddleSize), 1)
+        else:
+            screen.blit(ballImage, (ballX, ballY))
+            screen.blit(paddleImage, (40, player1Pos))
+            screen.blit(paddleImage, (720, player2Pos))
     #change colours
     r+=dr
     g+=dg
